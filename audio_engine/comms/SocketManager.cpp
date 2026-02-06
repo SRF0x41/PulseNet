e461fc8520a31db492e00f37d564c59e2a41a5e8
Sergio Rodriguez <sergio.rodriguezflores26@gmail.com>
2026-02-06 12:13:01 -0700

    added string loging to socket

--- audio_engine/comms/SocketManager.cpp
+++ audio_engine/comms/SocketManager.cpp
@@ -1,6 +1,7 @@
 #include "SocketManager.h"
 
 #include <cstring>
+#include <iostream>
 
 #ifdef _WIN32
 bool SocketManager::winsockInitialized = false;
@@ -19,8 +20,10 @@ SocketHandle SocketManager::invalidSocket() {
 // Correct definition
 void SocketManager::closeSocket(SocketHandle s) {
 #ifdef _WIN32
+  std::cout << "[SocketManager] Closing socket (Windows)\n";
   closesocket(s);
 #else
+  std::cout << "[SocketManager] Closing socket (POSIX)\n";
   ::close(s);
 #endif
 }
@@ -30,6 +33,7 @@ void SocketManager::ensureWinsock() {
   if (winsockInitialized)
     return;
 
+  std::cout << "[SocketManager] Initializing WinSock\n";
   WSADATA wsa;
   WSAStartup(MAKEWORD(2, 2), &wsa);
   winsockInitialized = true;
@@ -39,19 +43,25 @@ void SocketManager::ensureWinsock() {
 // ---------- Lifetime ----------
 
 SocketManager::SocketManager() {
+  std::cout << "[SocketManager] Constructor\n";
 #ifdef _WIN32
   ensureWinsock();
 #endif
 }
 
-SocketManager::~SocketManager() { disconnect(); }
+SocketManager::~SocketManager() {
+  std::cout << "[SocketManager] Destructor\n";
+  disconnect();
+}
 
 SocketManager::SocketManager(SocketManager &&other) noexcept {
+  std::cout << "[SocketManager] Move constructor\n";
   socketHandle = other.socketHandle;
   other.socketHandle = invalidSocket();
 }
 
 SocketManager &SocketManager::operator=(SocketManager &&other) noexcept {
+  std::cout << "[SocketManager] Move assignment\n";
   if (this != &other) {
     disconnect();
     socketHandle = other.socketHandle;
@@ -63,64 +73,91 @@ SocketManager &SocketManager::operator=(SocketManager &&other) noexcept {
 // ---------- Public API ----------
 
 bool SocketManager::connect(const std::string &host, uint16_t port) {
+  std::cout << "[SocketManager] connect(" << host << ":" << port << ")\n";
+
   disconnect();
 
   socketHandle = ::socket(AF_INET, SOCK_STREAM, 0);
-  if (socketHandle == invalidSocket())
+  if (socketHandle == invalidSocket()) {
+    std::cerr << "[SocketManager] socket() failed\n";
     return false;
+  }
 
   sockaddr_in addr{};
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);
 
 #ifdef _WIN32
-  if (InetPtonA(AF_INET, host.c_str(), &addr.sin_addr) != 1)
+  if (InetPtonA(AF_INET, host.c_str(), &addr.sin_addr) != 1) {
+    std::cerr << "[SocketManager] InetPtonA failed\n";
     return false;
+  }
 #else
-  if (inet_pton(AF_INET, host.c_str(), &addr.sin_addr) != 1)
+  if (inet_pton(AF_INET, host.c_str(), &addr.sin_addr) != 1) {
+    std::cerr << "[SocketManager] inet_pton failed\n";
     return false;
+  }
 #endif
 
-  if (::connect(socketHandle, reinterpret_cast<sockaddr *>(&addr),
+  if (::connect(socketHandle,
+                reinterpret_cast<sockaddr *>(&addr),
                 sizeof(addr)) < 0) {
+    std::cerr << "[SocketManager] connect() failed\n";
     disconnect();
     return false;
   }
 
+  std::cout << "[SocketManager] Connected successfully\n";
   return true;
 }
 
 void SocketManager::disconnect() {
   if (socketHandle != invalidSocket()) {
+    std::cout << "[SocketManager] disconnect()\n";
     closeSocket(socketHandle);
     socketHandle = invalidSocket();
   }
 }
 
 bool SocketManager::sendBytes(const void *data, size_t size) {
-  if (!isConnected())
+  if (!isConnected()) {
+    std::cerr << "[SocketManager] sendBytes() called while disconnected\n";
     return false;
+  }
+
+  std::cout << "[SocketManager] sendBytes(" << size << " bytes)\n";
 
 #ifdef _WIN32
-  return ::send(socketHandle, static_cast<const char *>(data),
-                static_cast<int>(size), 0) >= 0;
+  return ::send(socketHandle,
+                static_cast<const char *>(data),
+                static_cast<int>(size),
+                0) >= 0;
 #else
   return ::send(socketHandle, data, size, 0) >= 0;
 #endif
 }
 
 int SocketManager::receiveBytes(void *buffer, size_t size) {
-  if (!isConnected())
+  if (!isConnected()) {
+    std::cerr << "[SocketManager] receiveBytes() called while disconnected\n";
     return -1;
+  }
+
+  std::cout << "[SocketManager] receiveBytes(" << size << " bytes)\n";
 
 #ifdef _WIN32
-  return ::recv(socketHandle, static_cast<char *>(buffer),
-                static_cast<int>(size), 0);
+  return ::recv(socketHandle,
+                static_cast<char *>(buffer),
+                static_cast<int>(size),
+                0);
 #else
   return ::recv(socketHandle, buffer, size, 0);
 #endif
 }
 
 bool SocketManager::isConnected() const {
-  return socketHandle != invalidSocket();
+  bool connected = socketHandle != invalidSocket();
+  std::cout << "[SocketManager] isConnected() -> "
+            << (connected ? "true" : "false") << "\n";
+  return connected;
 }
--- audio_engine/main.cpp
+++ audio_engine/main.cpp
@@ -1,5 +1,6 @@
 #include "stream/AudioTrack.h"
 #include "stream/Streamer.h"
+#include "comms/SocketManager.h"
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_core/juce_core.h>
@@ -7,55 +8,60 @@
 
 int main() {
 
+  SocketManager socket;
+
+
+  return 0;
+
   /*TODO: make sure streamer removes tracks and fades in the timer callback! not
    * in the audio buffer callback*/
 
-  mixer::Streamer streamer;
-  AudioTrack trackA("/home/user1/Desktop/Dev/PulseNet/get_audio/tracks/Flume - Holdin On.mp3");
-  trackA.advanceStart(20);
-  trackA.setFadeInDuration(7);
-  trackA.setFadeOutDuration(5);
-  trackA.virtualEndTrim(27);
+  // mixer::Streamer streamer;
+  // AudioTrack trackA("/home/user1/Desktop/Dev/PulseNet/get_audio/tracks/Flume - Holdin On.mp3");
+  // trackA.advanceStart(20);
+  // trackA.setFadeInDuration(7);
+  // trackA.setFadeOutDuration(5);
+  // trackA.virtualEndTrim(27);
 
-  AudioTrack trackB("/home/user1/Desktop/Dev/PulseNet/get_audio/tracks/MDK.mp3");
-  trackB.setStartOverlapSeconds(29.571);
-  trackB.setFadeInDuration(5);
+  // AudioTrack trackB("/home/user1/Desktop/Dev/PulseNet/get_audio/tracks/MDK.mp3");
+  // trackB.setStartOverlapSeconds(29.571);                                                         
+  // trackB.setFadeInDuration(5);
 
-  streamer.addNext(trackA);
-  streamer.addNext(trackB);
-  streamer.start();
+  // streamer.addNext(trackA);
+  // streamer.addNext(trackB);
+  // streamer.start();
 
-  // --------------------
-  // Console input thread
-  // --------------------
-  std::thread inputThread([&streamer]() {
-    std::string userInput;
-    std::cout << "Type something (type 'exit' to quit):" << std::endl;
+  // // --------------------
+  // // Console input thread
+  // // --------------------
+  // std::thread inputThread([&streamer]() {
+  //   std::string userInput;
+  //   std::cout << "Type something (type 'exit' to quit):" << std::endl;
 
-    while (true) {
-      std::cout << "> ";
-      std::getline(std::cin, userInput);
+  //   while (true) {
+  //     std::cout << "> ";
+  //     std::getline(std::cin, userInput);
 
-      if (userInput == "exit") {
-        std::cout << "Exiting..." << std::endl;
-        streamer.stop(); // stop music playback
-        break;
-      }
+  //     if (userInput == "exit") {
+  //       std::cout << "Exiting..." << std::endl;
+  //       streamer.stop(); // stop music playback
+  //       break;
+  //     }
 
-      std::cout << "You typed: " << userInput << std::endl;
-    }
-  });
+  //     std::cout << "You typed: " << userInput << std::endl;
+  //   }
+  // });
 
-  // --------------------
-  // Run JUCE message loop in main thread
-  // --------------------
-  juce::MessageManager::getInstance()->runDispatchLoop();
+  // // --------------------
+  // // Run JUCE message loop in main thread
+  // // --------------------
+  // juce::MessageManager::getInstance()->runDispatchLoop();
 
-  // --------------------
-  // Wait for console thread to finish
-  // --------------------
-  if (inputThread.joinable())
-    inputThread.join();
+  // // --------------------
+  // // Wait for console thread to finish
+  // // --------------------
+  // if (inputThread.joinable())
+  //   inputThread.join();
 
-  return 0;
+  // return 0;
 }
