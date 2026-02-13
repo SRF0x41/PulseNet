c047f8752de39c6b4c2421f5b53850e98c25c48a
Sergio Rodriguez <sergio.rodriguezflores26@gmail.com>
2026-02-12 23:00:40 -0700

    added llm example

---
+++ frontend/llm_local_api.py
@@ -0,0 +1,11 @@
+from ollama import chat
+MODEL = 'llama2:13b'
+# Define a system prompt
+system_prompt = "You speaks and sounds like a pirate with short sentences."
+# Chat with a system prompt
+response = chat(MODEL, 
+                messages=[
+                    {'role': 'system', 'content': system_prompt},
+                    {'role': 'user', 'content': 'Tell me about your boat.'}
+                ])
+print(response.message.content)
